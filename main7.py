000efc2e20aac2ad67006aab3c4ea8049f987c6c
ramya0072005 <144790987+ramya0072005@users.noreply.github.com>
2023-09-12 15:35:35 +0530

    Add files via upload

---
+++ main7.py
@@ -0,0 +1,9 @@
+#factorial
+def fact_rec(n):
+  if n==0 or n==1:
+     return 1
+  else:
+    return n*fact_rec(n-1)
+number=int(input("enter a value:"))
+rec=fact_rec(number)
+print("the factorial of {} is {}:".format(number,rec))
